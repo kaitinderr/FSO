0b8c24e69292e31be0d1234357951c67f9f1dbce
kaitinderr <ktinder2003@gmail.com>
2023-03-23 15:03:08 -0700

    rename

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+    setTotal(left + right)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+    setTotal(left + right)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
+export default App
+
+// const Display = (props) => <div>{props.counter}</div>
+
+// const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+// const App = () => {
+//   const [ counter, setCounter ] = useState(0)
+
+//   const increaseByOne = () => setCounter(counter + 1)
+//   const decreaseByOne = () => setCounter(counter - 1)
+//   const setToZero = () => setCounter(0)
+
+//   return (
+//     <div>
+//       <Display counter={counter}/>
+//       <Button 
+//         handleClick={increaseByOne}
+//         text='plus'
+//       />
+//       <Button 
+//         handleClick={decreaseByOne}
+//         text='minus'
+//       />
+//       <Button 
+//         handleClick={setToZero}
+//         text='zero'
+//       />
+//     </div>
+//   )
+// }
