8888faa4a0466cce102fcb8790b1de9377bc71ff
kaitinderr <ktinder2003@gmail.com>
2023-03-25 14:14:13 -0700

    complete anecdote

--- part1/anecdote/src/App.js
+++ part1/anecdote/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Header = (props) => <h1>{props.text}</h1>
+
+const Button = (props) => <button onClick={props.handleClick}>{props.text}</button>
+
+const Anecdote = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{props.content}</p>
+      <p>has {props.votes} votes</p>
     </div>
-  );
+  )
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  console.log(votes)
+
+  console.log(selected)
+
+  const handleVoteClick = () => {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+  }
+
+  const handleAnecdoteClick = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+
+  const findMaxAnecdote = (votes) => {
+    const mostVotes = Math.max(...votes)
+    const index = votes.indexOf(mostVotes)
+    return index
+  }
+
+  return (
+    <div>
+      <Header text='Anecdote of the day' />
+      <Anecdote content={anecdotes[selected]} votes={votes[selected]} />
+      <Button handleClick={handleVoteClick} text='vote' />
+      <Button handleClick={handleAnecdoteClick} text='next anecdote' /> 
+      <Header text='Anecdote with most votes' />
+      <Anecdote content={anecdotes[findMaxAnecdote(votes)]} votes={votes[findMaxAnecdote(votes)]} />
+    </div>
+  )
+}
+
+export default App
